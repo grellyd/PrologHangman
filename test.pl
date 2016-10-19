9a7428d8e6a4070606029921767c6d3410ee5dac
Yasmeen Akbari <yasmeen.akbari@gmail.com>
2016-10-19 14:23:22 -0700

    got pl_number working

--- test.pl
+++ test.pl
@@ -1,19 +1,17 @@
-% pl_number(num) --> ['5'].
-pl_number(num) --> {integer(num)}.
-pl_number([H]) --> { integer(H)}.
-% pl_number(num) --> { integer(num) }.
-% pl_number(num) --> { float(num) }.
+pl_number(Num) --> { integer(Num) }.
+pl_number([H]) --> [H], pl_number(H).
+pl_number(Num) --> { float(Num) }.
+
 
 %% this rule doesnt compile
-%% pl_neg_number(negNum) --> 
-%% 
-%%     [H|num] = numberCodes(negNum),
-%%     true_neg_code = char_code('-'),
-%%     unknown_code = char_code(H),
-%%     true_neg_code = unknown_code,
-%%     pl_number(num).
+% pl_neg_number(NegNum) --> 
+%     [H|Num], numberCodes(NegNum),
+%     True_neg_code = char_code('-'),
+%     Unknown_code = char_code(H),
+%     True_neg_code = Unknown_code,
+%     pl_number(Num).
 
     
 % Testing...
-% phrase(pl_number(num), [5]).
+% phrase(pl_number(Num), [5]).
 % phrase(pl_number([H]), [5]).
