069b341fc9c3dafa4508e91e7f057ded76f20cc1
Yasmeen Akbari <yasmeen.akbari@gmail.com>
2016-10-23 12:37:48 -0700

    added initializing lives

--- hangman.pl
+++ hangman.pl
@@ -1,11 +1,14 @@
 % Hangman - CS 312 Project 1
 % Graham Brown, Tyler Young, Yasmeen Akbari
 
-%%%%%%%% TODO: start_hangman(WordCharList, PlayerProgList) can just be start_hangman, but putting the vars there
-%%%%%%%%		allows us to see the results for now
-start_hangman(WordCharList, PlayerProgList) :- init_word(WordCharList), init_player_view(WordCharList, PlayerProgList), play_game(WordCharList, PlayerProgList).
+%%%%%%%% TODO: start_hangman(WordCharList, PlayerProgList, Lives) can just be start_hangman, but putting the vars there
+%%%%%%%%		allows us to test the init values 
 
-play_game(WordCharList, PlayerProgList). % Probably turn this line into our "loop"
+get_lives(N) :- N is 6. % The number of wrong guesses Player 2 has
+start_hangman(WordCharList, PlayerProgList, Lives) :- init_word(WordCharList), init_player_view(WordCharList, PlayerProgList),
+												get_lives(Lives), play_game(Lives, WordCharList, PlayerProgList).
+
+play_game(Lives, WordCharList, PlayerProgList). % Probably turn this line into our "loop"
 
 
 % Prompts user to enter a word, and creates a list with the chars of the word
@@ -18,6 +21,7 @@ is_alpha_list([H|T]) :- char_type(H, alpha), is_alpha_list(T).
 is_alpha_list([]).
 
 % Create the list that displays player 2 progress
+% Example: if the word list is ['c', 'a', 't'] then the progress list is ['_', '_', '_']
 init_player_view(WordCharList, PlayerProgList) :- length(WordCharList, L), create_player_list(L, [], PlayerProgList). % maybe make copy and change it?
 
 create_player_list(0, PlayerProgList, PlayerProgList).
