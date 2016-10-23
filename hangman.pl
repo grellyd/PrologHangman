03d784ba1447be9f67d125d0ba88afc4e2fda048
Yasmeen Akbari <yasmeen.akbari@gmail.com>
2016-10-23 15:31:30 -0700

    added turns without dynamic args

--- hangman.pl
+++ hangman.pl
@@ -4,16 +4,27 @@
 %%%%%%%% TODO: start_hangman(WordCharList, PlayerProgList, Lives) can just be start_hangman, but putting the vars there
 %%%%%%%%		allows us to test the init values 
 
-get_lives(N) :- N is 6. % The number of wrong guesses Player 2 has
+get_lives(N) :- N is 6. % The number of wrong guesses Player 2 can make
 start_hangman(WordCharList, PlayerProgList, Lives) :- init_word(WordCharList), init_player_view(WordCharList, PlayerProgList),
 												get_lives(Lives), play_game(Lives, WordCharList, PlayerProgList).
 
-play_game(Lives, WordCharList, PlayerProgList). % Probably turn this line into our "loop"
+% This is the main loop
+% Ask Player 2 for a guess, check if it's right or wrong, update lives appropriately
+% Keeps prompting Player 2 until Lives reaches 0, or player has guessed the word
+%%%%%%%% TODO: check for winning conditions, probably compare WordCharList and PlayerProgList
+play_game(Lives, WordCharList, PlayerProgList) :- Lives > 0, write('Player 2, please enter your guess (single char, end with .):'), read(Guess),
+												update_progress(Lives, LivesAfter, WordCharList, PlayerProgListBefore, PlayerProgListAfter, Guess), 
+												play_game(LivesAfter, WordCharList,PlayerProgList).
+play_game(0, WordCharList, PlayerProgList) :- write('You lost!').
+
+update_progress(LivesBefore, LivesAfter, WordCharList, PlayerProgListBefore, PlayerProgListAfter, Guess):- member(Guess, WordCharList), LivesAfter is LivesBefore.
+update_progress(LivesBefore, LivesAfter, WordCharList, PlayerProgListBefore, PlayerProgListAfter, Guess):- \+ member(Guess, WordCharList), 
+																											LivesAfter is LivesBefore-1.
 
 
 % Prompts user to enter a word, and creates a list with the chars of the word
 %%%%%%%% TODO: is there a way to clear the console so player 2 can't see the input?
-init_word(WordCharList) :- write('Please enter a word for player 2 to guess (can\'t start uppercase, end with .): '),
+init_word(WordCharList) :- write('Player1, please enter a word for player 2 to guess (can\'t start uppercase, end with .): '),
 					read(Word), atom_chars(Word, WordCharList), is_alpha_list(WordCharList).
 					
 % Check the that the WordCharList is composed of only alpha chars
